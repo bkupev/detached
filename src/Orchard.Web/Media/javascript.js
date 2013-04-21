783ea371eb40fc0951dd68322f3dac73cd6a8b0b
blagoj <blagoj@bb.(none)>
2013-04-21 19:01:02 +0200

    first commit

---
+++ src/Orchard.Web/Media/javascript.js
@@ -0,0 +1,38 @@
+<script language="JavaScript">
+
+NewImg = new Array (
+"Media/why_off_the_grid.jpg",
+"Media/Latest_News.jpg",
+"Media/Events.jpg"
+);
+var ImgNum = 0;
+var ImgLength = NewImg.length - 1;
+//Time delay between Slides in milliseconds
+//var delay = 3000;
+//ar lock = false;
+//var run;
+/*
+function chgImg(direction) {
+    if (document.images) {
+        ImgNum = ImgNum + direction;
+        if (ImgNum > ImgLength) {
+            ImgNum = 0;
+        }
+        if (ImgNum < 0) {
+            ImgNum = ImgLength;
+        }
+        document.slideshow.src = NewImg[ImgNum];
+    }
+}
+function auto() {
+    if (lock == true) {
+        lock = false;
+        window.clearInterval(run);
+    }
+    else if (lock == false) {
+        lock = true;
+        run = setInterval("chgImg(1)", delay);
+    }
+}
+*/
+</script>
